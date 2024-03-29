9b11292fb15dd38d350c6f58cf5451a1af69dfd5
fenilsonani <21bmiit160@gmail.com>
2024-03-29 19:22:43 +0530

    company controller created and added data tables into project

---
+++ TPS/Controllers/CompanyController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TPS.Controllers
+{
+    public class CompanyController : Controller
+    {
+        // GET: /<controller>/
+
+        [ActionName("Index")]
+        public IActionResult Index()
+        {
+            // if session is not available then redirect to signin page
+            if (HttpContext.Session.GetString("role") == null || HttpContext.Session.GetString("role") != "1")
+            {
+                return RedirectToAction("SignIn", "Authentication");
+            }
+
+            return View("AllCompanies");
+        }
+    }
+}
+
